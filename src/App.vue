728fd1cf9ecd765d9ef561ee292b8a5df7ef2b81
tuananhp-1844 <pham.tuan.anh@sun-asterisk.com>
2019-09-06 16:04:49 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld 
+      image = "https://scontent.fhan2-4.fna.fbcdn.net/v/t39.1997-6/p240x240/68120606_410165212934465_9210642140336488448_n.png?_nc_cat=104&_nc_eui2=AeHkfXPNV-1cS5XtMrZ-LsWFbFSmQPplPv3FxWGVJr0Z19ZmL57tMvRrKFgmFUiujUCKuA1fk1iDrSWVu1OZ9hBIWULxs9_QKX_pXFTsUI2E-Q&_nc_oc=AQmBajqdmXeJ3QSZlSl_GYMu-xcuKu8aloKxGnMZM9pkshwyD6CW3OszB5yURriBnu0&_nc_ht=scontent.fhan2-4.fna&oh=f39b12ff10d2ffa84218d8958c8ab14d&oe=5E0A40E0"
+      :width="100"
+      :height="100"
+      :column="4"
+      :row="3"
+      :max="10"
+      :frame="1"
+    />
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,54 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div :style="style">
   </div>
 </template>
 
 <script>
+import { clearInterval } from 'timers';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    image: String,
+    height: Number,
+    width: Number,
+    loop: Number,
+    frame: Number,
+    max: Number,
+    column: Number,
+    row: Number,
+  },
+
+  mounted() {
+    this.play()
+  },
+
+  data(){
+    return {
+      style: {
+        width: this.width + 'px',
+        height: this.height + 'px',
+        background: `url(${this.image})`,
+        backgroundSize: `${this.width * this.column}px ${this.height * this.row}px`,
+        backgroundPosition: '0px 0px'
+      }
+    }
+  },
+  
+  methods: {
+    play() {
+      let i = 0
+      setInterval(() => {
+        if(i % this.column) {
+          this.$set(this.style, 'backgroundPosition', `${this.width * i}px 0px`)
+        } else {
+          this.$set(this.style, 'backgroundPosition', `0px ${this.height * i}px`)
+        }
+        if (i == this.max) {
+          i = 0
+        }
+        i++;
+      }, 1000/this.frame);
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
